a3fbc4ee4cbe0ee960c4238649f1fdde994e68c6
Arthur <timeiqu@gmail.com>
2022-07-29 14:57:55 +0300

    Add sha2

--- encryption.rb
+++ encryption.rb
@@ -4,10 +4,11 @@ require "digest"
 puts "Введите фразу:"
 s = gets.chomp
 puts "Способ шифрования:"
-puts "1. MD5\n2. SHA1"
+puts "1. MD5\n2. SHA1\n3. SHA2"
 method = gets.to_i
 
 case method
 when 1 then puts Digest::MD5.hexdigest s
 when 2 then puts Digest::SHA1.hexdigest s
+when 3 then puts Digest::SHA2.hexdigest s
 end
